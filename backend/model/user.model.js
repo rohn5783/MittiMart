86928f3004b7de2d2f4bc46acef6acd87991e4a4
Rohitweb3 <rp5120523@gmail.com>
2026-06-27 11:44:59 +0530

    role based access implementing started

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -16,6 +16,13 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             trim: true,
         },
+        role: {
+            type: String,
+            required: true,
+            enum: ['buyer', 'seller', 'admin'],
+            default: 'buyer'
+
+        },
         password: {
             type: String,
             required: true,
